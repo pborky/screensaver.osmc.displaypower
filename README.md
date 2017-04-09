14ce2ab01cd9f3af71cd4e60ee7ca68ef2b1c0e8
Mirko Romstadt <mirko@romstadt.net>
2017-04-09 15:24:48 +0200

    Initial commit for OSMC

---
+++ README.md
@@ -0,0 +1,9 @@
+# OSMC Display Power
+
+This addon is for OSMC *only*!
+
+It runs `cec-client` command to turn off hdmi power and force the display to go to standby mode.
+
+Shamelessly forked from [asavah](https://github.com/asavah/script.pidisplaypower).
+
+Icon made by [Dave Gandy](http://www.flaticon.com/authors/dave-gandy) from [Flaticon](http://www.flaticon.com) is licensed by [Creative Commons BY 3.0](http://creativecommons.org/licenses/by/3.0/).
--- resources/lib/gui.py
+++ resources/lib/gui.py
@@ -23,19 +23,21 @@ class Screensaver(xbmcgui.WindowXMLDialog):
 
     def _power_toggle(self):
         if self._is_powered:
-            arg2 = '0'
+            cmd1 = ['echo', 'standby', '0'] 
         else:
-            arg2 = '1'
+            cmd1 = ['echo', 'on', '0'] 
         try:
-            cmd = subprocess.Popen(['vcgencmd', 'display_power', arg2], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            (out, err) = cmd.communicate()
-            xbmc.log(msg="%s: vcgencmd returned %s" % ( __addonid__, repr(out) ), level=xbmc.LOGDEBUG)
+            cmd2 = ['/usr/osmc/bin/cec-client', '-d', '1', '-s']
+            ps1 = subprocess.Popen((cmd1), stdout=subprocess.PIPE)
+            ps2 = subprocess.Popen((cmd2), stdin=ps1.stdout, stdout=subprocess.PIPE)
+            (out, err) = ps2.communicate()
+            xbmc.log(msg="%s: cec-client returned %s" % ( __addonid__, repr(out) ), level=xbmc.LOGDEBUG)
             if not err:
                 self._is_powered = not self._is_powered
             else:
-                xbmc.log(msg="%s: vcgencmd returned %s" % ( __addonid__, repr(err) ), level=xbmc.LOGERROR)
+                xbmc.log(msg="%s: cec-client returned %s" % ( __addonid__, repr(err) ), level=xbmc.LOGERROR)
         except:
-            xbmc.log(msg="%s: Exception running vcgencmd" % ( __addonid__ ), level=xbmc.LOGERROR)
+            xbmc.log(msg="%s: Exception running cec-client" % ( __addonid__ ), level=xbmc.LOGERROR)
 
 
 class MyMonitor(xbmc.Monitor):
@@ -44,3 +46,4 @@ class MyMonitor(xbmc.Monitor):
 
     def onScreensaverDeactivated(self):
         self.action()
+
